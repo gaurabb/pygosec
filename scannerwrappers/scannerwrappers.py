1aee56bf480d917f411cc6109b8dc6e01423c56d
Gaurab Bhattacharjee <gaurabb@hotmail.com>
2017-03-14 12:10:10 -0700

    Scanner run methods moved to a different module

---
+++ scannerwrappers/scannerwrappers.py
@@ -0,0 +1,32 @@
+
+import subprocess
+
+class ScannerWraps:
+
+    '''
+    The method [runsafesql] will be used to run safesql on the
+    '''
+
+    def runsafesql(PATH_TO_CODE_TO_SCAN):
+
+        safesql_success_message = "You're safe from SQL injection! Yay \o/"
+        starry_line="***************"
+        straight_line="------------------"
+        try:
+            print("\nINFO:Running [safesql]...")
+            safesql_run = subprocess.Popen(["safesql", PATH_TO_CODE_TO_SCAN], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            print("INFO: Processing the results...")
+            safesql_return_code = safesql_run.wait()
+            safesql_result = safesql_run.stdout.read().decode("utf-8")
+            if (safesql_return_code == 0):
+                if safesql_result.strip() == safesql_success_message:
+                    print("{0}\nNO ISSUES DETECTED\n{3}\n[safesql] scan result for GO project, {1}: \n{2}\n{0}".format(starry_line, PATH_TO_CODE_TO_SCAN, safesql_result, straight_line))
+                else:
+                    print("{0}\nISSUES DETECTED\n{3}\n[safesql] scan result for GO project, {1}: \n{2}\n{0}".format(starry_line, PATH_TO_CODE_TO_SCAN, safesql_result, straight_line))
+            else:
+                print("ERROR: [safesql] exit with an error code {0} and following message \n{1}".format(safesql_return_code,
+                                                                                                          safesql_result))
+        except:
+            raise
+
+
